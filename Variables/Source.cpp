31a08d4cbe88a6ff9acca6dd36a3115b5aba9a92
RalenskiDoucet <Ralenskid@gmail.com>
2017-08-23 12:57:46 -0500

    Intail commit

---
+++ Variables/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+void main()
+{
+	//You can not print to the console unless the problem specifies.
+
+	//1. Select suitable variable types and names for the following examples
+	//a) The average height of students in your class
+	int avgHight = 6;
+	//b) The tax rate for incomes over $65000
+	int taxRate = 65000;
+	//c) The tax rate for incomes below $5000
+	int rateOftax = 5000;
+	//d) The total cost of all the games on your Steam library
+	int totalOfall = 29;
+
+	//2. Select suitable data types to hold the following values :
+	//a) 121
+	int;
+	//b) 9.4
+	float;
+	//c) r
+	char;
+	//d) 100000
+	int;
+	//e) False
+	bool;
+
+	//3. Create a program that swaps two numbers. Ask the user for 2 numbers in the console window
+	//and store the result in variables called “a” and “b” print the result to screen.Write code 
+	//that swaps the values for these variables, then print the variables “a” and “b” again.
+	//You should comment your code.
+	int a = 5;
+	int b = 10;
+	int c = a;
+	b = a;
+	b = c;
+	//4. Ask the user for 5 numbers and output the average of these numbers.	
+	int numberOne = 75;
+	int numberTwo = 100;
+	std::cin >> numberOne;
+	std::cin >> numberTwo;
+
+	numberOne,+numberTwo=
+	
+	
+	numberOne + numberTwo=125
+	int numberThree = 100;
+	int numberFour = 125;
+	int numberFive = 150;
+	int avgOfnumbers = 100;
+	std::cout << (numberOne, numberTwo , +numberThree, +numberFour, +numberFive | 500 % 5, avgOfnumbers = 100);
+	avgOfnumbers = 100;
+
+		//5. Redo question 3 but allows the user to input the values to be swapped and provide information
+		//in the console proving the data was swapped.
+
+}
